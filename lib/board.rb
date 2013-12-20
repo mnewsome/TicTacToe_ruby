4abc86bbbf78f982f9bf261fa66b005dbb995c02
Dave Moore <dave@8thlight.com>
2013-12-20 16:56:21 -0600

    Blowing Malcolm's Mind!!!!

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_reader :spaces
+
+  def initialize(size)
+    @spaces = Array.new(size, "")
+  end
+
+  def size
+    @spaces.size
+  end
+
+  def get_value_at(space)
+    @spaces[space + 1]
+  end
+
+  def fill_space_at(space, value)
+    @spaces[space + 1] = value
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,22 @@
+require "rspec"
+require "board"
+
+describe Board do
+  before(:each) do
+    @board = Board.new(9)
+  end
+
+  it "creates spaces based on number inputted" do
+    Board.new(9).size.should == 9
+    Board.new(3).size.should == 3
+  end
+
+  it "every space is empty by default" do
+    @board.get_value_at(1).should be_empty
+  end
+
+  it "every space can be filled" do
+    @board.fill_space_at(1, "some arbitrary value")
+    @board.get_value_at(1).should == "some arbitrary value"
+  end
+end
